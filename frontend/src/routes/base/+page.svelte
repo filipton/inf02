5618a8970766a238bc4e6fd6968a00044df713ad
filipton <filipton12@gmail.com>
2023-04-04 23:10:55 +0200

    feat: base lazyload

--- frontend/src/routes/base/+page.svelte
+++ frontend/src/routes/base/+page.svelte
@@ -6,16 +6,57 @@
     import QuestionElement from "$lib/components/QuestionElement.svelte";
 
     let allQuestions: Question[] = [];
+    let shownQuestions: Question[] = [];
 
     questions.subscribe(async (x) => {
         if (!browser) return;
 
         allQuestions = x;
+        lazyLoad();
     });
+
+    function onScroll() {
+        if (shownQuestions.length >= allQuestions.length) return;
+
+        const lastQuestion = document.querySelector(
+            ".question:last-child"
+        ) as HTMLElement;
+        if (isInViewport(lastQuestion)) {
+            lazyLoad();
+        }
+    }
+
+    function lazyLoad(count: number = 100) {
+        let questions = allQuestions.slice(0, shownQuestions.length + count);
+        shownQuestions = questions;
+    }
+
+    function isInViewport(element: HTMLElement) {
+        const rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <=
+                (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <=
+                (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
 </script>
 
+<svelte:window on:scroll={onScroll} />
+
 <QuestionsHandler>
-    {#each allQuestions as question, i}
+    {#if shownQuestions.length < allQuestions.length}
+        <button
+            class="w-full h-12 bg-gray-600 text-white mb-8 rounded"
+            on:click={() => lazyLoad(2227)}
+        >
+            LOAD EVERYTHING (BIG LAG)
+        </button>
+    {/if}
+
+    {#each shownQuestions as question, i}
         <QuestionElement
             {question}
             questionNumber={i + 1}
