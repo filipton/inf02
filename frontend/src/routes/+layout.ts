a50a12ca4dd7b1b8cc1b5044aaaabacf1678e334
filipton <filipton12@gmail.com>
2023-04-14 22:49:33 +0200

    feat: better cache

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -3,7 +3,9 @@ import type { Question } from '$lib/types';
 import type { LayoutLoad, LayoutLoadEvent } from './$types';
 
 export const load = (async (event: LayoutLoadEvent) => {
-    const response = await event.fetch("/base.json");
+    const response = await event.fetch("/base.json", {
+        cache: "no-cache"
+    });
     let _questions: Question[] = await response.json();
 
     questions.set(_questions);
