34ac92ac986cb9c460f6834341461325e5a13832
filipton <filipton12@gmail.com>
2023-04-30 20:39:27 +0200

    feat: base search

--- frontend/src/routes/base/+page.svelte
+++ frontend/src/routes/base/+page.svelte
@@ -5,15 +5,40 @@
     import QuestionsHandler from "$lib/components/QuestionsHandler.svelte";
     import QuestionElement from "$lib/components/QuestionElement.svelte";
 
+    let base: Question[] = [];
     let allQuestions: Question[] = [];
     let shownQuestions: Question[] = [];
+    let searchQuery = "";
 
     questions.subscribe(async (x) => {
         if (!browser) return;
 
+        base = x;
         allQuestions = x;
         lazyLoad();
     });
+
+    function search() {
+        let query = searchQuery.toLowerCase();
+        if (query === "") {
+            shownQuestions = base.slice(0, 50);
+            return;
+        }
+
+        allQuestions = base.filter(
+            (x) =>
+                x.text.toLowerCase().includes(query) ||
+                x.anwsers.some((a) => a.toLowerCase().includes(query)) ||
+                (x.id + 1).toString() === query
+        );
+        allQuestions = allQuestions.sort((a, b) => {
+            if ((a.id + 1).toString() === query) return -1;
+            if ((b.id + 1).toString() === query) return 1;
+            return 0;
+        });
+
+        shownQuestions = allQuestions.slice(0, 50);
+    }
 
     function onScroll() {
         if (shownQuestions.length >= allQuestions.length) return;
@@ -36,19 +61,20 @@
 <svelte:window on:scroll={onScroll} />
 
 <QuestionsHandler>
-    {#if shownQuestions.length < allQuestions.length}
-        <button
-            class="w-full h-12 bg-gray-600 text-white mb-8 rounded"
-            on:click={() => lazyLoad(2227)}
-        >
-            LOAD EVERYTHING (HUGE LAG)
-        </button>
-    {/if}
+    <div class="mb-8">
+        <input
+            type="text"
+            placeholder="Search (Number, Question, Anwser)"
+            class="w-full px-4 py-2 text-white bg-gray-900 rounded"
+            bind:value={searchQuery}
+            on:input={search}
+        />
+    </div>
 
-    {#each shownQuestions as question, i}
+    {#each shownQuestions as question}
         <QuestionElement
             {question}
-            questionNumber={i + 1}
+            questionNumber={question.id + 1}
             ended={true}
             showDidntChoose={false}
         />
--- frontend/src/routes/tests/starred-all/+page.svelte
+++ frontend/src/routes/tests/starred-all/+page.svelte
@@ -1,1 +1,1 @@
-<h1 class="text-2xl font-bold">Jeszcze nie zrobione!</h1>
+<h1 class="text-2xl font-bold text-center w-full text-white">Jeszcze nie zrobione!</h1>
