dac007600834f52830e17c3f4f8c9ad46a0aa2d4
filipton <filipton12@gmail.com>
2023-05-29 18:37:33 +0200

    fix(docs-creator): recursive

--- docs-creator/src/main.rs
+++ docs-creator/src/main.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use serde::Serialize;
 
 #[derive(Debug, Serialize)]
@@ -11,8 +13,19 @@ const FRONTEND_STATIC_DIR: &str = "../frontend/static";
 
 fn main() -> anyhow::Result<()> {
     let mut entries: Vec<DocsEntry> = Vec::new();
+    read_reccursive(&PathBuf::from(DOCS_GITHUB_DIR), &mut entries);
 
-    for entry in std::fs::read_dir(DOCS_GITHUB_DIR).unwrap() {
+    let json = serde_json::to_string_pretty(&entries).unwrap();
+    std::fs::write(
+        format!("{}/docs.json", FRONTEND_STATIC_DIR),
+        json.as_bytes(),
+    )?;
+
+    Ok(())
+}
+
+fn read_reccursive(path: &PathBuf, entries: &mut Vec<DocsEntry>) {
+    for entry in std::fs::read_dir(path).unwrap() {
         let entry = entry.unwrap();
         let path = entry.path();
         let file_name = path.file_name().unwrap().to_str().unwrap();
@@ -26,14 +39,8 @@ fn main() -> anyhow::Result<()> {
 
             let markdown = std::fs::read_to_string(path).unwrap();
             entries.push(DocsEntry { url, markdown });
+        } else if path.is_dir() {
+            read_reccursive(&path, entries);
         }
     }
-
-    let json = serde_json::to_string_pretty(&entries).unwrap();
-    std::fs::write(
-        format!("{}/docs.json", FRONTEND_STATIC_DIR),
-        json.as_bytes(),
-    )?;
-
-    Ok(())
 }
